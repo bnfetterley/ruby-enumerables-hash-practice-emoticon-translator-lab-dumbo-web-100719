f39b86d6f24315b35b0e6528250524733a4cedb5
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 23:47:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,18 +23,19 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   translation = load_library(file_path)
   if !translation["get_emoticon"][emoticon]
-    return "Sorry, that emoticon was not found"
-  else translation["get_emoticon"][emoticon]
+     "Sorry, that emoticon was not found"
+  else 
+    translation["get_emoticon"][emoticon]
 end
-  #     if emoticon == english
-  #       puts [japanese]
-  #     end
-  #   end
-  # code goes here
 end
 
 def get_english_meaning
-  # code goes here
+   translation = load_library(file_path)
+  if !translation["get_meaning"][emoticon]
+     "Sorry, that emoticon was not found"
+  else 
+    translation["get_meaning"][emoticon]
+end
 end
 
 #value.each do |new_value, names|
