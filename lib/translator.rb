153e1dba3008fcea559c6f9b4f958928b4611628
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 23:48:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
    translation = load_library(file_path)
   if !translation["get_meaning"][emoticon]
      "Sorry, that emoticon was not found"
