85baa1c407ca59c80877cdafdae0889f8e5a7cbf
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 23:16:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,15 +16,12 @@ def load_library(file_path)
   
   new_hash["get_emoticon"][english] = japanese 
   new_hash["get_meaning"][japanese] = key
-
-#binding.pry  
-
 end
-#binding.pry
 new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
+  load_library
   # code goes here
 end
 
